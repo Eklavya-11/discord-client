5da88e6bdfa33af7107150c50d1ebfbff921ec20
Eklavya11 <117552685+Eklavya-11@users.noreply.github.com>
2023-03-29 12:13:05 +0200

    Update message.js

--- test/message.js
+++ test/message.js
@@ -1,1 +1,23 @@
-// todo
+// a basic usage
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const { Client } = require('../index'); // Setting our client here
+bot.cli = new Client(bot);
+
+bot.on('ready', () => {
+    bot.cli.setGame('I\'m online !');
+    console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+        bot.cli.message.create({
+            to: msg.channel.id,
+            content: 'Pong!'
+        });
+    }
+});
+
+bot.login('token');
