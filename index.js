bd8b6e6bf903e4d9230ffc36d274e4d323de5d3d
Eklavya11 <117552685+Eklavya-11@users.noreply.github.com>
2023-03-29 12:01:11 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//
+
+module.exports = { 
+    Client: require('./src/Client'),
+    Message: require('./src/Message'),
+    PlayerManager: require('./src/PlayerManager'),
+    Utils: require('./src/Utils')
+};
